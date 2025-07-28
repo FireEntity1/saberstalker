e14f7f06e5e41f8f0f133a325ce1aee1fea5434e
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-07-28 15:45:59 -0600

    chore: setup (it finally responds)

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-require('dotenv').config();
 const { App } = require('@slack/bolt');
 
+require('dotenv').config();
+
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
-    port: process.env.PORT || 3000,
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN
+});
+
+app.message('hello', async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
   });
-  
-  app.message(async ({ message, say }) => {
-    await say(`Got your message, <@${message.user}>!`);
-  });
-  
-  (async () => {
-    await app.start();
-  
-    app.logger.info('⚡️ Bolt app is running!');
-  })();
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+  app.logger.info('App running');
+})();
