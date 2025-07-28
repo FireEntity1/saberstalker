944276f842874690b665ca454d62c8140748e775
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2025-07-28 15:14:31 -0600

    chore: node

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { App } = require('@slack/bolt');
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
+    port: process.env.PORT || 3000,
+  });
+  
+  app.message(async ({ message, say }) => {
+    await say(`Got your message, <@${message.user}>!`);
+  });
+  
+  (async () => {
+    await app.start();
+  
+    app.logger.info('⚡️ Bolt app is running!');
+  })();
